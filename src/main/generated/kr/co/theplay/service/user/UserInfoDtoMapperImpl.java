b13b70b06d21ee743d67f1547f2d475b59fa6a86
epicurean21 <jm_company@naver.com>
2021-03-10 21:12:16 +0900

    For Deploy

---
+++ src/main/generated/kr/co/theplay/service/user/UserInfoDtoMapperImpl.java
@@ -0,0 +1,47 @@
+package kr.co.theplay.service.user;
+
+import javax.annotation.Generated;
+import kr.co.theplay.domain.user.User;
+import kr.co.theplay.domain.user.UserRole;
+import kr.co.theplay.dto.user.UserInfoDto;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-03-10T20:43:32+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 15.0.1 (AdoptOpenJDK)"
+)
+public class UserInfoDtoMapperImpl implements UserInfoDtoMapper {
+
+    @Override
+    public User toEntity(UserInfoDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String email = null;
+        String nickname = null;
+
+        id = dto.getId();
+        email = dto.getEmail();
+        nickname = dto.getNickname();
+
+        String password = null;
+        UserRole userRole = null;
+
+        User user = new User( id, email, password, nickname, userRole );
+
+        return user;
+    }
+
+    @Override
+    public UserInfoDto toDto(User entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        UserInfoDto userInfoDto = new UserInfoDto();
+
+        return userInfoDto;
+    }
+}
