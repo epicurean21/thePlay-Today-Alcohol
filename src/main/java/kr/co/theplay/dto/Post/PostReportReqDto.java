45c9384211d1547eec919fd5a54536aaf49d3b5b
epicurean21 <jm_company@naver.com>
2021-03-25 02:09:56 +0900

    게시물 신고, 게시물 작성 시 신고 횟수로  작성 제한, 사용자 화면 상단 정보
    - 개발 완료

--- src/main/java/kr/co/theplay/dto/Post/PostReportReqDto.java
+++ src/main/java/kr/co/theplay/dto/Post/PostReportReqDto.java
@@ -11,7 +11,7 @@ public class PostReportReqDto {
     @ApiModelProperty(value = "게시글 번호", dataType = "Integer", required = true, example = "1")
     private Long postId;
 
-    @ApiModelProperty(value = "신고 이유", dataType = "String", required = true, example = "스팸")
+    @ApiModelProperty(value = "신고 이유", dataType = "String", required = true, example = "스팸: 0, 선정적: 1, 부적절: 2 ")
     private String content;
 
     @Builder
