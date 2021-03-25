4a81b5b0b7c97b18139e3d7c6e90c9f47682b337
Hae-Riri <ehl3288@naver.com>
2021-03-25 21:55:05 +0900

    게시글 api
    - 수정 API 개발

--- src/main/java/kr/co/theplay/api/PostController.java
+++ src/main/java/kr/co/theplay/api/PostController.java
@@ -91,4 +91,21 @@ public class PostController {
         return new ResponseEntity<>(result, HttpStatus.OK);
 
     }
+
+/*    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-ACCESS-TOKEN", value = "Access Token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "게시글 수정", notes = "로그인한 사용자가 본인의 게시글을 수정한다.")
+    @PutMapping(value = "/v1/post/{postId}")
+    public ResponseEntity<CommonResult> updatePost(@PathVariable("postId") Long postId){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+        if (email.equals("anonymousUser")) {
+            throw new CommonConflictException("accessException");
+        }
+
+        postService.updatePost()
+    }*/
 }
