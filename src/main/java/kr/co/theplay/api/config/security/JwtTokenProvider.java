fd697f6ba8c6c9932343fdf0920b5ae35991d38e
epicurean21 <jm_company@naver.com>
2021-03-28 16:49:12 +0900

    JWT 수정
    - 개발 완료

--- src/main/java/kr/co/theplay/api/config/security/JwtTokenProvider.java
+++ src/main/java/kr/co/theplay/api/config/security/JwtTokenProvider.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,10 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
@@ -46,7 +42,7 @@ public class JwtTokenProvider {
                 .setClaims(claims) // 정보 저장
                 .setIssuedAt(now) // 토큰 발행 시간 정보
                 .setExpiration(new Date(now.getTime() + tokenValidTime)) // 만료 시간
-                .signWith(SignatureAlgorithm.HS384, secretKey) // 사용할 암호와 알고리즘 + Secrete Key
+                .signWith(SignatureAlgorithm.HS256, secretKey) // 사용할 암호와 알고리즘 + Secrete Key
 
                 .compact();
     }
